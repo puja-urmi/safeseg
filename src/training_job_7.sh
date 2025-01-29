37a6f6ee96621b27650dbbb821025d8004311004
Puja Saha <psaha03@uoguelph.ca>
2025-01-29 10:39:24 -0800

    Added and updated job submission bash files for sequential training job for total 400 rounds approx, changed workspace directory into scratch.

---
+++ src/training_job_7.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=p100:1
+#SBATCH --cpus-per-task=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32000M
+#SBATCH --time=2880:00
+#SBATCH --output=training_%J.log   
+
+# Load the required modules
+module load python/3.11
+python3 -m venv /home/psaha03/scratch/safeseg/env
+source /home/psaha03/scratch/safeseg/env/bin/activate
+
+# Display GPU information
+nvidia-smi
+
+# Execute the Python script
+pip install -r requirements.txt
+python3 -u -m nvflare.private.fed.app.simulator.simulator './configs/brats_central' -w '/home/psaha03/scratch/workspace_brats/brats_central' -n 1 -t 1 -gpu 0
