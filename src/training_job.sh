1a8d10bcecb597cc7f914cf4f5bedfd066c4608f
puja saha <pujaurmi.saha@gmail.com>
2025-01-29 00:39:32 -0500

    updated previous necessary things.

---
+++ src/training_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=p100:1
+#SBATCH --cpus-per-task=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32000M
+#SBATCH --time=120:00
+#SBATCH --output=training_%J.log   
+
+# Load the required modules
+module load python/3.11
+python3 -m venv /home/psaha03/scratch/FSBAKT/env
+source /home/psaha03/scratch/FSBAKT/env/bin/activate
+
+# Display GPU information
+nvidia-smi
+
+# Execute the Python script
+pip install -r requirements.txt
+mkdir ./workspace_brats
+python3 -u -m nvflare.private.fed.app.simulator.simulator './configs/brats_central' -w './workspace_brats/brats_central' -n 1 -t 1 -gpu 0
