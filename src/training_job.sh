26665eaa1c8b64fa5dcad64a59aa261e2b1f158f
Puja Saha <psaha03@uoguelph.ca>
2025-02-10 09:39:16 -0800

    updated configs and bash files for 20 rounds and 20 hours. Renamed some files as well

--- src/training_job.sh
+++ src/training_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32000M
-#SBATCH --time=60:00
+#SBATCH --time=1230:00
 #SBATCH --output=training_%J.log   
 
 # Load the required modules
@@ -17,5 +17,5 @@ nvidia-smi
 
 # Execute the Python script
 pip install -r requirements.txt
-mkdir /home/psaha03/scratch/workspace_kits
-python3 -u -m nvflare.private.fed.app.simulator.simulator './configs/kits_central' -w '/home/psaha03/scratch/workspace_kits/kits_central' -n 1 -t 1 -gpu 0
+mkdir /home/psaha03/scratch/workspace_kits_central
+python3 -u -m nvflare.private.fed.app.simulator.simulator './configs/kits_central' -w '/home/psaha03/scratch/workspace_kits_central/kits_central' -n 1 -t 1 -gpu 0
