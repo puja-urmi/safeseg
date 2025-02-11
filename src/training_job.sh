13232a5b9651c064eea3b7bae49b86779d31ea8e
Puja Saha <psaha03@uoguelph.ca>
2025-02-10 22:07:36 -0800

    created new branch bras24-fed

--- src/training_job.sh
+++ src/training_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=p100:1
+#SBATCH --gpus-per-node=p100:3
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32000M
-#SBATCH --time=1230:00
+#SBATCH --time=120:00
 #SBATCH --output=training_%J.log   
 
 # Load the required modules
@@ -17,5 +17,5 @@ nvidia-smi
 
 # Execute the Python script
 pip install -r requirements.txt
-mkdir /home/psaha03/scratch/workspace_brats_central
-python3 -u -m nvflare.private.fed.app.simulator.simulator './configs/brats_central' -w '/home/psaha03/scratch/workspace_brats_central/brats_central' -n 1 -t 1 -gpu 0
+mkdir /home/psaha03/scratch/workspace_brats_fedavg
+python3 -u -m nvflare.private.fed.app.simulator.simulator './configs/brats_fedavg' -w '/home/psaha03/scratch/workspace_brats_fedavg/brats_fedavg' -n 5 -t 5 -gpu 0
