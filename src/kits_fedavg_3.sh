d929f81328049535166585d5bf3c5c4ddca03473
Puja Saha <psaha03@uoguelph.ca>
2025-02-12 12:29:22 -0800

    removed some unnecessary things for this branch

--- src/kits_fedavg_3.sh
+++ src/kits_fedavg_3.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32000M
-#SBATCH --time=7000:00
+#SBATCH --time=4-00:00:00
 #SBATCH --output=kits_fedavg_3_%J.log   
 
 # Load the required modules
@@ -18,4 +18,4 @@ nvidia-smi
 # Execute the Python script
 pip install -r requirements.txt
 mkdir /home/psaha03/scratch/workspace_kits_fedavg_3
-nvflare simulator './configs/kits_fedavg' -w './workspace_kits_fedavg_3/kits_fedavg' -n 3 -t 3 -gpu 0,1,2
+nvflare simulator './configs' -w '/home/psaha03/scratch/workspace_kits_fedavg_3' -n 2 -t 2 -gpu 0,1
--- src/kits_fedavg_4.sh
+++ src/kits_fedavg_4.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32000M
-#SBATCH --time=7000:00
+#SBATCH --time=4-00:00:00
 #SBATCH --output=kits_fedavg_4_%J.log   
 
 # Load the required modules
@@ -18,4 +18,4 @@ nvidia-smi
 # Execute the Python script
 pip install -r requirements.txt
 mkdir /home/psaha03/scratch/workspace_kits_fedavg_4
-nvflare simulator './configs/kits_fedavg' -w './workspace_kits_fedavg_4/kits_fedavg' -n 4 -t 4 -gpu 0,1,2,3
+nvflare simulator './configs' -w '/home/psaha03/scratch/workspace_kits_fedavg_4' -n 2 -t 2 -gpu 0,1
