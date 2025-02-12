6a02beafa8f073a55101eae476407cb2ed48592e
Puja Saha <psaha03@uoguelph.ca>
2025-02-12 10:09:16 -0800

    created 3 bash files for different client numbers

--- src/kits_fedavg_2.sh
+++ src/kits_fedavg_2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=p100:4
+#SBATCH --gpus-per-node=p100:2
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32000M
 #SBATCH --time=120:00
-#SBATCH --output=kits_fedavg_%J.log   
+#SBATCH --output=kits_fedavg_2_%J.log   
 
 # Load the required modules
 module load python/3.11
@@ -17,5 +17,5 @@ nvidia-smi
 
 # Execute the Python script
 pip install -r requirements.txt
-mkdir /home/psaha03/scratch/workspace_kits_fedavg
-nvflare simulator './configs/brats_fedavg' -w './workspace_brats/brats_fedavg' -n 4 -t 4 -gpu 0,1,2,3
+mkdir /home/psaha03/scratch/workspace_kits_fedavg_2
+nvflare simulator './configs/kits_fedavg' -w './workspace_kits_fedavg_2/kits_fedavg' -n 2 -t 2 -gpu 0,1
---
+++ src/kits_fedavg_3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=p100:3
+#SBATCH --cpus-per-task=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32000M
+#SBATCH --time=7000:00
+#SBATCH --output=kits_fedavg_3_%J.log   
+
+# Load the required modules
+module load python/3.11
+python3 -m venv /home/psaha03/scratch/safeseg/env
+source /home/psaha03/scratch/safeseg/env/bin/activate
+
+# Display GPU information
+nvidia-smi
+
+# Execute the Python script
+pip install -r requirements.txt
+mkdir /home/psaha03/scratch/workspace_kits_fedavg_3
+nvflare simulator './configs/kits_fedavg' -w './workspace_kits_fedavg_3/kits_fedavg' -n 3 -t 3 -gpu 0,1,2
