d679f8f740413d510f83f186b337b2bc32fa4148
Puja Saha <psaha03@uoguelph.ca>
2025-02-19 07:22:04 -0800

    Reduced roi, increased lr for faster convergence.

--- src/brats_central.sh
+++ src/brats_central.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32000M
-#SBATCH --time=15-00:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --output=brats_central_%J.log   
 
 # Load the required modules
