4826bcdc770418ed23234e42a39e728fbd28aa87
Puja Saha <psaha03@uoguelph.ca>
2025-02-25 12:57:47 -0800

    updated testing script according to training script.

--- src/result_stat/testing_models_3d.sh
+++ src/result_stat/testing_models_3d.sh
@@ -1,11 +1,17 @@
-workspace_path="../workspace_brats"
-dataset_path="../dataset_brats18/dataset"
-datalist_path="../dataset_brats18/datalist"
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=v100l:1
+#SBATCH --cpus-per-task=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32000M
+#SBATCH --time=0-02:00:00
+#SBATCH --output=brats_central_test_%J.log   
+
+
+
+workspace_path="/home/psaha03/scratch/workspace_brats_central"
+dataset_path="/home/psaha03/scratch/dataset_brats24/dataset"
+datalist_path="/home/psaha03/scratch/dataset_brats24/datalist"
 
 echo "Centralized"
-python3 brats_3d_test_only.py --model_path "${workspace_path}/brats_central/simulate_job/app_server/best_FL_global_model.pt" --dataset_base_dir ${dataset_path} --datalist_json_path "${datalist_path}/site-All.json"
-echo "FedAvg"
-python3 brats_3d_test_only.py --model_path "${workspace_path}/brats_fedavg/simulate_job/app_server/best_FL_global_model.pt" --dataset_base_dir ${dataset_path} --datalist_json_path "${datalist_path}/site-All.json"
-echo "FedAvgDP"
-python3 brats_3d_test_only.py --model_path "${workspace_path}/brats_fedavg_dp/simulate_job/app_server/best_FL_global_model.pt" --dataset_base_dir ${dataset_path} --datalist_json_path "${datalist_path}/site-All.json"
-
+python3 brats_3d_test_only.py --model_path "${workspace_path}/server/simulate_job/app_server/best_FL_global_model.pt" --dataset_base_dir ${dataset_path} --datalist_json_path "${datalist_path}/site-test.json"
