144b421185047f128c0c1e310468b8dc585f921e
Puja Saha <psaha03@uoguelph.ca>
2025-02-26 08:40:09 -0800

    added bash file to train on 2 clients

---
+++ src/kits_fedavg_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=v100l:1
+#SBATCH --cpus-per-task=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32000M
+#SBATCH --time=2-00:00:00
+#SBATCH --output=kits_fedavg_2_%J.log   
+
+# Load the required modules
+module load python/3.11
+python3 -m venv /home/psaha03/scratch/safeseg/env
+source /home/psaha03/scratch/safeseg/env/bin/activate
+
+# Display GPU information
+nvidia-smi
+
+# Execute the Python script
+pip install -r requirements.txt
+mkdir /home/psaha03/scratch/workspace_kits_fedavg_2
+nvflare simulator './configs' -w '/home/psaha03/scratch/workspace_kits_fedavg_2' -n 2 -t 2 -gpu 0,0
